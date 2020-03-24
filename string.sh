c2702de198db8255c73b9c6483ad10fcf340bdf8
Nathan Rajlich <nathan@tootallnate.net>
2020-03-24 12:05:20 -0700

    Add `string_includes`, `string_starts_with`, and `string_ends_with`

--- string.sh
+++ string.sh
@@ -17,3 +17,34 @@ string_sub() {
 string_gsub() {
   flags=g string_sub "$@"
 }
+
+# https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
+string_includes() {
+  local r=1
+  case "$1" in
+    *"$2"*)
+      r=0
+      ;;
+  esac
+  return "$r"
+}
+
+string_starts_with() {
+  local r=1
+  case "$1" in
+    "$2"*)
+      r=0
+      ;;
+  esac
+  return "$r"
+}
+
+string_ends_with() {
+  local r=1
+  case "$1" in
+    *"$2")
+      r=0
+      ;;
+  esac
+  return "$r"
+}
--- string_test.sh
+++ string_test.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
-set -xeuo pipefail
-. ./string.sh
+set -euo pipefail
+eval "$(curl -sfLS import.pw)"
+import "assert@2.1.2"
+source ./string.sh
 
-echo aabbcc | string_sub b d
-echo aabbcc | string_sub b '/'
-echo aabbcc | string_gsub b '/'
+# string_lower
+assert_equal "$(echo AaBbCc123 | string_lower)" "aabbcc123"
+
+# string_upper
+assert_equal "$(echo AaBbCc123 | string_upper)" "AABBCC123"
+
+# string_sub
+assert_equal "$(echo aabbcc | string_sub b d)" "aadbcc"
+assert_equal "$(echo aabbcc | string_sub b '/')" "aa/bcc"
+
+# string_gsub
+assert_equal "$(echo aabbcc | string_gsub b '/')" "aa//cc"
+
+# string_includes
+assert_exit 0 string_includes "abc" "a"
+assert_exit 0 string_includes "abc" "b"
+assert_exit 0 string_includes "abc" "ab"
+assert_exit 0 string_includes "abc" "bc"
+assert_exit 1 string_includes "abc" "d"
+
+# string_starts_with
+assert_exit 0 string_starts_with "abc" "a"
+assert_exit 1 string_starts_with "abc" "b"
+assert_exit 0 string_starts_with "/home/me/foo.txt" "/home/me"
+assert_exit 1 string_starts_with "/root/foo.txt" "/home/me"
+
+# string_ends_with
+assert_exit 1 string_ends_with "abc" "a"
+assert_exit 1 string_ends_with "abc" "b"
+assert_exit 0 string_ends_with "abc" "c"
+assert_exit 0 string_ends_with "abc" "bc"
+
+echo "Tests passed!"
