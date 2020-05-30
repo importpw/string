de813663b7ee11c2a2fa28df15c5b217928fdee6
Nathan Rajlich <nathan@tootallnate.net>
2020-05-30 01:09:32 -0700

    Add `.editorconfig`

--- string.sh
+++ string.sh
@@ -1,50 +1,50 @@
 string_lower() {
-  tr '[:upper:]' '[:lower:]'
+	tr '[:upper:]' '[:lower:]'
 }
 
 string_upper() {
-  tr '[:lower:]' '[:upper:]'
+	tr '[:lower:]' '[:upper:]'
 }
 
 string_sub() {
-  local replace
-  local with
-  replace="$(echo "$1" | sed 's|/|\\/|g')"
-  with="$(echo "$2" | sed 's|/|\\/|g')"
-  sed 's/'"$replace"'/'"$with"'/'"${flags-}"
+	local replace
+	local with
+	replace="$(echo "$1" | sed 's|/|\\/|g')"
+	with="$(echo "$2" | sed 's|/|\\/|g')"
+	sed 's/'"$replace"'/'"$with"'/'"${flags-}"
 }
 
 string_gsub() {
-  flags=g string_sub "$@"
+	flags=g string_sub "$@"
 }
 
 # https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
 string_includes() {
-  local r=1
-  case "$1" in
-    *"$2"*)
-      r=0
-      ;;
-  esac
-  return "$r"
+	local r=1
+	case "$1" in
+		*"$2"*)
+			r=0
+			;;
+	esac
+	return "$r"
 }
 
 string_starts_with() {
-  local r=1
-  case "$1" in
-    "$2"*)
-      r=0
-      ;;
-  esac
-  return "$r"
+	local r=1
+	case "$1" in
+		"$2"*)
+			r=0
+			;;
+	esac
+	return "$r"
 }
 
 string_ends_with() {
-  local r=1
-  case "$1" in
-    *"$2")
-      r=0
-      ;;
-  esac
-  return "$r"
+	local r=1
+	case "$1" in
+		*"$2")
+			r=0
+			;;
+	esac
+	return "$r"
 }
